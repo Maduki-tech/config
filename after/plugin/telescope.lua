634cab41d7a80fcaf8bd54676529f6d857d18aeb
Maduki-tech <d.schlueter1011@gmail.com>
2023-10-13 09:12:36 +0200

    better telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -32,9 +32,9 @@ vim.keymap.set(
     {desc = "[/] Fuzzily search in current buffer"}
 )
 
-vim.keymap.set("n", "<leader>gf", require("telescope.builtin").git_files, {desc = "Search [G]it [F]iles"})
+vim.keymap.set("n", "<leader>fg", require("telescope.builtin").git_files, {desc = "Search [G]it [F]iles"})
 vim.keymap.set("n", "<leader>fs", require("telescope.builtin").find_files, {desc = "[F]iles [S]earch "})
-vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, {desc = "[S]earch [H]elp"})
+vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags, {desc = "[S]earch [H]elp"})
 vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, {desc = "[S]earch current [W]ord"})
 vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, {desc = "[S]earch by [G]rep"})
 vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, {desc = "[S]earch [D]iagnostics"})
